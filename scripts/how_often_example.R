257da00cb78687a303192fd56e3d92fc0d444b0f
Wojciech Kretowicz <32495076+WojciechKretowicz@users.noreply.github.com>
2018-11-04 18:04:56 +0100

    Added function how_often_per_word()

--- scripts/how_often_example.R
+++ scripts/how_often_example.R
@@ -5,6 +5,6 @@ library(data.table)
 library(ggplot2)
 library(stringi)
 
-temp <- how_often(scripts, regex = "sam")
+temp <- how_often_per_word(scripts, regex = "(ring|treasure|precious)")
 
 ggplot(data = temp, aes(x = char, y = average)) + geom_col() + coord_flip()
