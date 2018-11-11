7e980efd029ae0e6b0436794fcbbd054ba0af31c
WojciechKretowicz <wojtekkretowicz@gmail.com>
2018-11-11 19:16:19 +0100

    Skala log10 w x

--- scripts/wykres_nr_1.R
+++ scripts/wykres_nr_1.R
@@ -9,6 +9,8 @@ library(grid)
 library(ggrepel)
 library(extrafont)
 library(extrafontdb)
+library(ggpubr)
+library(ggimage)
 
 df <- merge(how_often_per_word(scripts, regex = "( ring| precious)"), how_often_per_word(scripts, regex = "( mine| my| want)"), by = 'char', all.x = FALSE, all.y = FALSE)
 
@@ -22,7 +24,7 @@ df <- df[,.(char,ring,mine,
 med.x <- median(df$ring)
 med.y <- median(df$mine)
 
-labels.x <- paste(seq(0.00, 0.021, 0.002),"%",sep = "")
+labels.x <- paste(round(rep(2,10) ^ (-14:-5),digits=3),"%",sep = "")
 labels.y <- paste(seq(0.00, 0.015, 0.001),"%",sep = "")
 
 #link do pobrania czcionki https://dl.1001fonts.com/ringbearer.zip
@@ -37,12 +39,12 @@ P <- ggplot(data = df, aes(x = ring, y = mine, label = char, color = race)) +
   #annotation_custom(rasterGrob(img, width=unit(1,"npc"), height=unit(1,"npc")), -Inf, Inf, -Inf, Inf) + 
   geom_point(color = 'white', size = 3) + geom_label_repel(size = 5, force = 15, segment.color = 'white', family="Ringbearer", label.padding = .6,
                                                  label.size = 3) + geom_hline(yintercept = med.y, color = 'white') + geom_vline(xintercept = med.x, color = 'white') +
-  annotate("label", x = 0.018, y = 0.0165, label = 'Anxious', size = 12, color = 'red', family="Ringbearer") + 
-  annotate('label', x = 0.018, y = 0.001, label = 'Interested', size = 12, color = 'orange', family="Ringbearer") +
+  annotate("label", x = 0.016, y = 0.0165, label = 'Anxious', size = 12, color = 'red', family="Ringbearer") + 
+  annotate('label', x = 0.016, y = 0.001, label = 'Interested', size = 12, color = 'orange', family="Ringbearer") +
   annotate('label', x = 0.0016, y = 0.001, label = 'Neutral', size = 12, color = 'green', family="Ringbearer") +
   annotate('label', x = 0.0016, y = 0.0165, label = 'Greedy', size = 12, color = 'orange', family="Ringbearer") + 
   xlab('Frequency of mentioning the Ring') + ylab('Frequency of utter \"my/mine\"') +
-  scale_x_continuous(breaks = seq(0.00, 0.021, 0.002), labels = labels.x) +
+  scale_x_log10(breaks = rep(2,10) ^ (-14:-5), labels = labels.x) +
   scale_y_continuous(breaks = seq(0.00, 0.015, 0.001), labels = labels.y) +
   ggtitle("How hard did Gollum covet the ring?") +
   theme(title = element_text(color = 'gold', size = 20),axis.title = element_text(size = 20, color = 'gold'),
